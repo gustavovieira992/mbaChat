d1db6512c6ded8af5e2d286f8bafb62700046c21
gustavovieira <gustavovieira992@gmail.com>
2019-02-18 08:12:05 -0300

    first commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,12 @@
+export const appconfig = {
+  firebase: {
+    apiKey: "AIzaSyDUD9DKKlKFRFcGkIV8dmLYpYH-kEwNUw0",
+    authDomain: "mba-chat-d7e4b.firebaseapp.com",
+    databaseURL: "https://mba-chat-d7e4b.firebaseio.com",
+    projectId: "mba-chat-d7e4b",
+    storageBucket: "mba-chat-d7e4b.appspot.com",
+    messagingSenderId: "367330430830"
+  },
+  users_endpoint: "users",
+  chats_endpoint: "chats"
+};
